71cb79f887e58ead16944d8ef8c9f451a3a7d371
KateMcGeath <kate.a.mcgeath-1@ou.edu>
2019-10-07 17:23:02 -0500

    Completed the calAverage method in the MesoInherit class.

--- src/MesoInherit.java
+++ src/MesoInherit.java
@@ -4,6 +4,7 @@
  */
 public class MesoInherit extends MesoAbstract {
 
+	private static final int ID_LENGTH = 4;
 	private String stID;
 	private String[] stationList;
 	
@@ -14,6 +15,22 @@ public class MesoInherit extends MesoAbstract {
 	
 	@Override
 	public int[] calAverage() {
+		double avg = 0.0;
+		int[] avgArray = new int[3];
+		
+		//Looping through the stationList array to add together the ASCII values. 
+		//Converts the string to character to turn the char to an ascii value.
+		for(int index = 0; index < ID_LENGTH; index++) {
+			char character = stationList[index].charAt(0);
+			avg = avg + (int)character;
+		} avg = avg / ID_LENGTH;
+		
+		//Calculate the different ascii averages.
+		avgArray[0] = (int)Math.ceil(avg);
+		avgArray[1] = (int)Math.floor(avg);
+		avgArray[2] = (int)Math.round(avg);
+		
+		return avgArray;
 		
 	}
 	
