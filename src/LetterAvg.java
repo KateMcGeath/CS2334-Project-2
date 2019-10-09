a07792b1c74ac3ff8062daf610c8dc8dc6d36a0b
KateMcGeath <kate.a.mcgeath-1@ou.edu>
2019-10-09 18:46:28 -0500

    Updating comments and making them more specific for the LetterAvg class.

--- src/LetterAvg.java
+++ src/LetterAvg.java
@@ -7,30 +7,40 @@ public class LetterAvg {
 	private String[] stationList = new String[10];
 	private char avg;
 	
+	//LetterAvg Constructor.
 	public LetterAvg(char c) {
 		this.avg = c;
 	}
 	
 	public int numberOfStationsWithLetterAvg() throws IOException {
+		
+		//Initializing variables.
 		PosAvg posAvg = new PosAvg();
 		this.stationList = posAvg.getStationsArray();
 		int stations = 0;
-			for(int index = 0; index < stationList.length; index++) {
-				if(stationList[index] != null && stationList[index].charAt(0) == avg)
-					stations++;
-			} return stations;
+		
+		//Using a for loop to loop through the stations array and comparing each station's 
+		//first letter to the letter average.
+		for(int index = 0; index < stationList.length; index++) {
+			if(stationList[index] != null && stationList[index].charAt(0) == avg)
+				stations++;
+		} return stations;
 	}
 	
 	public String toString() {
+		
+		//Initializing variables.
 		PosAvg posAvg = new PosAvg();
 		String output = "They are:";
 		try {
 			this.stationList = posAvg.getStationsArray();
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
+			//Auto-generated catch block
 			e.printStackTrace();
 		}
 		
+		//Using a for loop to loop through the stations array and comparing each station's 
+		//first letter to the letter average, and then adding those stations to the output.
 		for(int index = 0; index < stationList.length; index++) {
 			if(stationList[index] != null && stationList[index].charAt(0) == avg)
 				output = output + "\n" + stationList[index];
